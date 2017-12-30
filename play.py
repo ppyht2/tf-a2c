a667aa11e9f52744269b9c007f4430d867cf71b5
ppyht2 <ppyht2@gmail.com>
2017-12-30 23:42:04 +0000

    cleaned play.py

--- play.py
+++ play.py
@@ -15,17 +15,11 @@ def get_args():
 
 def get_model(env, nsteps=5, nstack=4, total_timesteps=int(80e6),
               vf_coef=0.5, ent_coef=0.01, max_grad_norm=0.5, lr=7e-4,
-              lrschedule='linear', epsilon=1e-5, alpha=0.99, gamma=0.99, log_interval=100):
-
-    ob_space = env.observation_space
-    ac_space = env.action_space
-    # Load model
-    model = Model(policy=Policy, ob_space=ob_space, ac_space=ac_space, nenvs=1,
-                  nsteps=nsteps, nstack=1, num_procs=1,
+              epsilon=1e-5, alpha=0.99, gamma=0.99, log_interval=100):
+    model = Model(policy=Policy, ob_space=env.observation_space,
+                  ac_space=env.action_space, nenvs=1, nsteps=nsteps, nstack=1,
                   ent_coef=ent_coef, vf_coef=vf_coef, max_grad_norm=max_grad_norm,
-                  lr=lr, alpha=alpha, epsilon=epsilon, total_timesteps=total_timesteps,
-                  lrschedule=lrschedule)
-
+                  lr=lr, alpha=alpha, epsilon=epsilon, total_timesteps=total_timesteps)
     return model
 
 
